f82e6cae459ba0f6e66b8d41096866589e90821d
Adam Spice <adam@spicey.dev>
2025-09-29 16:11:18 +1300

    Add alt attribute to AvatarImage for accessibility

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -18,7 +18,7 @@ const UserAvatar = ({ name, imageUrl, size = "small" }: UserAvatarProps) => {
   return (
     <div>
       <Avatar className={sizeClasses[size]}>
-        <AvatarImage src={imageUrl} />
+        <AvatarImage src={imageUrl} alt={`${name}'s image`} />
         <AvatarFallback className="bg-yellow-500 text-sm font-bold text-yellow-900">
           {name[0]}
         </AvatarFallback>
